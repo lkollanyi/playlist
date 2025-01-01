69c427e639eb4f820e03abc5567183798061242a
lkollanyi <lkollanyi@gmail.com>
2025-01-01 23:37:33 +0100

    Styled static playlist component

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import TrackList from '../TrackList/TrackList';
+import styles from './Playlist.module.css'
 
 export default function Playlist() {
   return (
-    <div>
-        <h2>Playlist name (editable)</h2>
+    <div className={styles.container}>
+        <h2 className={styles.title}>Playlist name (editable)</h2>
         <TrackList/>
-        <button>Save Playlist</button>
+        <button className={styles.btn}>Save Playlist</button>
     </div>
   )
 }
